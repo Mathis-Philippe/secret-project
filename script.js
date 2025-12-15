202ea327bbe05a7287586ab14dc5fa5891b9aa34
Mathis <mathis.philippe@my-digital-school.org>
2025-12-15 16:31:26 +0100

    fix/ animation ne se joue qu'une fois

--- script.js
+++ script.js
@@ -8,16 +8,17 @@ addEventListener('keydown', function(e) {
     if (e.code === 'Space') {
         if (isKeyDown) return;
             isKeyDown = true;
-            if (isAnimationRunning === false) {
+            if (isAnimationRunning) return;
                 isAnimationRunning = true;
                 main.style.transform = "translate(-50%, calc(-50% - 150px))";
                 setTimeout(() => {
                     main.style.transform = "translate(-50%, -50%)";
+                    point++;
+                    displayScore.textContent = `Score : ${point}`;
+                    setTimeout(() => {
+                        isAnimationRunning = false; 
+                    }, 200);
                 }, 180);
-                point++;
-                displayScore.textContent = `Score : ${point}`;
-            }
-        isAnimationRunning = false;
                 
     }
 });
